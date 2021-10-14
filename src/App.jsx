274717fe849dfb5c827051f848fb3329c66de6dd
Timple Basinger <tbasinger@protonmail.com>
2021-10-14 11:17:25 -0500

    initial

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import GreaterThing from '.relative/Greeter.jsx';
+
+// const App = () => {
+
+//     return <h1>Unless you want to go another round,
+//         one of us is going to have to leave,
+// //         and I was here first.</h1>;
+// };
+
+const FavoriteQuote = props => {
+    return (
+        <h2>One of my favorite quotes from The Mandalorian by {name} is {phrase}
+        </h2>
+    );
+};
+
+
+
+export default FavoriteQuote;
---
+++ src/Greeter.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const GreeterThing = () => {
+    return (
+        <div>
+            <FavoriteQuote name="Mando" phrase="I'll see you again. I promise." />
+            <FavoriteQuote name="Cara" phrase="Pay up, Mudscuffers." />
+            <FavoriteQuote name="The Mandalorian" phrase="Stop touching things." />
+            <FavoriteQuote name="Cobb Vanth" phrase="I guess every once in a while both suns shing on a womp rat's tail." />
+            </ div>
+            );
+};
+
+           
